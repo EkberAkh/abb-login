e1e6c11b71d6656f3acae4e9485e0ceb5607038a
EkberAkh <ekber.0303@gmail.com>
2023-12-15 08:14:56 +0400

    added folder structure

---
+++ src/app/(pages)/onboarding/application/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+//erize sehifesi
+
+const Application = () => {
+  return <div>Application</div>;
+};
+
+export default Application;
---
+++ src/app/(pages)/onboarding/asan-imza-pin-3/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AsanImzaPin3 = () => {
+  return (
+    <div>AsanImzaPin3</div>
+  )
+}
+
+export default AsanImzaPin3
