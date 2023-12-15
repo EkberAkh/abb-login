ec0d84076faee5bf673ba4f4e302c8dcd8e3af5b
Murad Israfilov <muradisrafilov542@gmail.com>
2023-12-15 23:09:17 +0400

    adding components folder

--- src/app/(pages)/onboarding/asan-imza-pin-1/page.tsx
+++ src/app/(pages)/onboarding/asan-imza-pin-1/page.tsx
@@ -1,16 +1,14 @@
-import { Box, ChakraProvider, Container, Heading, Text, Wrap } from '@chakra-ui/react'
+import { Box,  Container, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const AsanImzaPinOne = () => {
   return (
-    <div>
       <Container as='div'>
         <Box as='div'>
           <Heading textAlign='center'>ASAN İmza təsdiqi</Heading>
           <Text>Telefonunuza daxil olan kodu aşağıdakı kodu ilə eyni olmasını müqayisə edin və ASAN PIN1 ilə təsdiqləyin.</Text>
         </Box>
       </Container>
-    </div> 
   )
 }
 
