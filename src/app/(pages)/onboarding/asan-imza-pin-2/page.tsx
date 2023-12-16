f4f80349b6c0c796504cc21b7847b862cba3e8a3
Murad Israfilov <muradisrafilov542@gmail.com>
2023-12-16 15:10:58 +0400

    Create TSX structure and apply Chakra styling to ASAN PIN components

--- src/app/(pages)/onboarding/asan-imza-pin-2/page.tsx
+++ src/app/(pages)/onboarding/asan-imza-pin-2/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client'
+import { Box,  Container, Heading, Progress, Text, Button, Image, CloseButton, Stack, VStack } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 const AsanImzaPinTwo = () => {
+
+  const [progressValue, setProgressValue] = useState(100);
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgressValue(prevValue => Math.max(prevValue - (100 / 1000), 0));
+    }, 10);
+    setTimeout(() => {
+      clearInterval(timer);
+      router.push('/onboarding');
+    }, 10000); 
+  }, [router]);
+
+  const clickHandler = () => {
+    router.push('/onboarding');
+  };
+
   return (
-    <div>AsanImzaPinTwo</div>
+    <Stack position='relative'>
+    <CloseButton onClick={clickHandler} position='absolute' right='24px' top='24px' background='#EDF2F7'  _hover={{ backgroundColor: 'gray.200'}} />
+      <Container as='div' display='flex' justifyContent='center' flexDirection='column' height='100vh' gap='16px' maxW='500px' px='24px'>
+          <VStack><Image src='/abb_logo.svg' alt='logo'/></VStack>
+          <Heading textAlign='center' color="#000" fontSize='24px' fontWeight='600' lineHeight='30px'>ASAN İmza təsdiqi</Heading>
+          <Text textAlign='center' fontSize='16px' fontWeight='400' lineHeight='24px' color='rgba(0, 0, 0, 0.50)'>Telefonunuza daxil olan kodu aşağıdakı kodu ilə eyni olmasını müqayisə edin və ASAN PIN2 ilə təsdiqləyin.</Text>
+          <Box border='1px solid #E4E4E4' borderRadius='12px'>
+            <Box as='div' p='24px' textAlign='center'>
+              <Text color='rgba(0, 0, 0, 0.50)' fontSize='14px' fontWeight='500' lineHeight= '20px' mb='4px'>Yoxlama kodu</Text>
+              <Text color='#000' fontSize='24px' fontWeight='600' lineHeight= '32px'>2123</Text>
+            </Box>
+            <Progress value={progressValue} borderRadius='full' />
+          </Box>
+          <Button colorScheme='white' color='black' w="100%" onClick={clickHandler}  _hover={{ backgroundColor: 'gray.100' }}>İmtina et</Button>
+      </Container>
+    </Stack>
   )
 }
 
-export default AsanImzaPinTwo
+export default AsanImzaPinTwo;
--- src/app/(pages)/onboarding/asan-imza-pin-3/page.tsx
+++ src/app/(pages)/onboarding/asan-imza-pin-3/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client'
+import { Box,  Container, Heading, Text, Image, CloseButton, Stack, Alert, AlertIcon, VStack } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation';
 
-const AsanImzaPin3 = () => {
+const AsanImzaPinThree = () => {
+  const router = useRouter();
+  const clickHandler = () => {
+    router.push('/onboarding');
+  }
   return (
-    <div>AsanImzaPin3</div>
+    <Stack position='relative'>
+      <VStack mt='40px' w='100%' position='absolute'>
+      <Alert borderRadius='6px' maxWidth='504px' status='error'>
+        <AlertIcon />
+        Hörmətli müştəri, müştəri məlumatlarınız üzrə düzəliş gərəklidir. Məsələ ilə bağlı ABB-yə məxsus filial və ya şöbəyə müraciət etməyiniz xahiş olunur.
+      </Alert>
+      </VStack>
+      <CloseButton onClick={clickHandler} position='absolute' right='24px' top='24px' background='#EDF2F7'  _hover={{ backgroundColor: 'gray.200'}} />
+      <Container as='div' display='flex' justifyContent='center' flexDirection='column' height='100vh' gap='16px' maxW='500px' px='24px'>
+          <VStack ><Image src='/abb_logo.svg' alt='logo'/></VStack>
+          <Heading textAlign='center' color="#000" fontSize='24px' fontWeight='600' lineHeight='30px'>Sorğunuz icradadır</Heading>
+          <Text textAlign='center' fontSize='18px' fontWeight='400' lineHeight='28px' color='#4A5568'>Hörmətli müştəri, qısa müddət ərzində prosesinin nəticəsi ilə bağlı mail vasitəsi ilə Sizə məlumat veriləcəkdir.</Text>
+      </Container>
+    </Stack>
   )
 }
 
-export default AsanImzaPin3
+export default AsanImzaPinThree;
