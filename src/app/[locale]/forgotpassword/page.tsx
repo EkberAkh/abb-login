d734e1fda4dd76b2a563b28ce45a671f00f1bb5d
taliieva <teranealieva01@gmail.com>
2023-12-17 23:19:54 +0400

    forgotpassword added

--- src/app/[locale]/forgotpassword/page.tsx
+++ src/app/[locale]/forgotpassword/page.tsx
@@ -1,12 +1,47 @@
-import { Layout } from '@/components/Layout';
-import React from 'react'
+import { Layout } from "@/components/Layout";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import {
+  VStack,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import React from "react";
 
 const forgotPassword = () => {
   return (
     <Layout>
-        <h1>Terane</h1>
+      <VStack padding="80px" alignItems="start" width="100%">
+        <Text mb="32px" fontWeight="700">
+          Şifrəmi unutdum
+        </Text>
+        <FormControl gap="24px">
+          <FormLabel mb="6px" fontWeight="500">
+            İstifadəçi adı
+          </FormLabel>
+          <Input type="text" borderColor="gray.300" />
+        </FormControl>
+        <Button
+          width="100%"
+          padding="10px 24px"
+          backgroundColor="#2058BB"
+          color="white"
+          fontSize="18px"
+          lineHeight="28px"
+          fontWeight="500"
+          mt="16px"
+        >
+          Şifrəni yenilə
+        </Button>
+        <Button colorScheme="teal" variant="ghost" width="100%" mt="16px" color="#2058BB" padding="10px 24px">
+       
+          Girişə qayıdın
+        </Button>
+      </VStack>
     </Layout>
-  )
-}
+  );
+};
 
 export default forgotPassword;
