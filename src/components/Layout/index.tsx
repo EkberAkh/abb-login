d472ce4e2b814c4036d87c5e3cfd3f1dbe5ed776
Sama Zeynalli <sema.kazimli@gmail.com>
2023-12-18 03:48:56 +0400

    UI

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Flex, HStack, VStack } from "@chakra-ui/react";
+import { Box, Flex, HStack, VStack, Text, Image } from "@chakra-ui/react";
 import React from "react";
 import { Header } from "./Header";
 import Footer from "./Footer";
@@ -14,7 +14,7 @@ export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
         justifyContent="space-between"
       >
         <Header />
-        <Flex>{children}</Flex>
+        <Flex w='100%'>{children}</Flex>
         <Footer/>
       </VStack>
       <Box
@@ -22,9 +22,35 @@ export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
         display={{ base: "none", lg: "none", xl: "flex" }}
         minH="100vh"
         justifyContent="stretch"
-        bg="blue.400"
+        bg="blue.600"
+        position='relative'
       >
-        Right Side
+        <Text 
+          color='white' 
+          fontSize='40px' 
+          position='relative'
+          top='46px' left='40px'>
+        ABB business tətbiqi ilə {" "}
+        <span style={{ display: "block", fontWeight: "bold" }}>işinizi irəli aparın!</span>
+        </Text>
+        <Image 
+        position='absolute'
+        left='40px'
+        top='177px'
+        alt="QR1" 
+        src="/login/images/G2roup 2.svg" />
+        <Image 
+        position='absolute'
+        left='170px'
+        top='177px'
+        alt="QR2" 
+        src="/login/images/Group 121.svg" />
+
+        <Image 
+        position='absolute'
+        right='80px'
+        bottom='5px'
+        src='/login/images/ABB_2business_phone2 1.svg'></Image>
       </Box>
     </HStack>
   );
