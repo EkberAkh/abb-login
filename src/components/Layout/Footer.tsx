095ecc202b23c13f97be633b1378510894988c3f
Rufat Gadirov <gadirov.567@gmail.com>
2023-12-23 19:24:20 +0400

    fixed footer and formPhone onBlur

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,24 +1,33 @@
 import { Layout } from "@/components/Layout";
 import { HStack, Image, Link } from "@chakra-ui/react";
 import { useTranslations } from "next-intl";
-
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Footer = () => {
   const t = useTranslations();
+  const pathName = usePathname();
+
+
+  const getUrl = () => {
+    if (pathName === "/az") {
+      return "https://asanimza.az/";
+    } else if (pathName === "/en") {
+      return "https://asanimza.az/en/";
+    } else {
+      return "https://asanimza.az/";
+    }
+  };
+
+  
   return (
-    <HStack
-      w="100%"
-      justify="space-between"
-      align="center"
-      h="87px"
-      px="80px"
-    >
-      <Image src="/login/images/asan-logo.svg" />
-      <Link fontSize="14px" color="gray.500" fontWeight="400">
-        {t("login.howToGetAsanImza")}
-      </Link>
-    </HStack>
+<HStack minWidth="465px" justify="flex-start" align="center" h="87px" _hover={{ color: 'red' }}>
+  <Image src="/login/images/asan-logo.svg" />
+  <Link fontSize="14px" color="gray.500" fontWeight="400" href={getUrl()} target="_blank" pl="20px" _hover={{ color: 'blue' }}>
+    {t("login.howToGetAsanImza")}
+  </Link>
+</HStack>
+
   );
 };
 
